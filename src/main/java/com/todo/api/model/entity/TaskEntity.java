c5a25c79246a7721ab074c450eb696f738cc9cab
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-16 16:14:02 +0300

    refactor: reformat TaskEntity based on Google Java Format
    I do that just to see if it will be included in the list of unformatted files from a format-check job logs.

--- src/main/java/com/todo/api/model/entity/TaskEntity.java
+++ src/main/java/com/todo/api/model/entity/TaskEntity.java
@@ -13,17 +13,20 @@ import java.time.LocalDateTime;
 @Table(name = "tasks")
 public class TaskEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false, length = 50)
-    private String title;
-    @Column(nullable = false, length = 500)
-    private String description;
-    @Column(nullable = false)
-    private LocalDateTime dueDate;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private TaskStatus taskStatus;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
+  @Column(nullable = false, length = 50)
+  private String title;
+
+  @Column(nullable = false, length = 500)
+  private String description;
+
+  @Column(nullable = false)
+  private LocalDateTime dueDate;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private TaskStatus taskStatus;
 }
